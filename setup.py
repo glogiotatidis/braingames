2730580936749571d93a894c8502e4660f79580f
Giorgos Logiotatidis <seadog@sealabs.net>
2017-02-08 18:21:46 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='braingames',
+    version='0.1dev',
+    description='This is https://github.com/mozilla/braingames',
+    author='Mozilla Foundation',
+    author_email='',
+    url='https://github.com/mozilla/braingames'
+)
