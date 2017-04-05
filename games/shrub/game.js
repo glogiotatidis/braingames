d80366ae50945ca8fa578483f2c76e9b19d9960d
Josh Gaunt <jgaunt@mozilla.com>
2017-04-05 14:39:11 -0700

    made language changes suggested in rrayborn feedback

--- games/shrub/game.js
+++ games/shrub/game.js
@@ -107,8 +107,9 @@ var instructions_block = {
     "<p>That understanding can improve web experiences by<br>tailoring them around data contributed through your participation</p>" +
     "<p>Left and right arrow keys navigate<br>back and forward through the instructions</p>",
 
-    "<p><strong>Please do NOT do any of the following things,<br>or you will be disqualified to prevent confounding results:</strong></p>" +
-    "<ul><li>Hit ESC to exit fullscreen mode</li><li>Switch tabs or programs during the procedure</li><li>Zoom in or out</li><li>Reload the page once you have begun trials</li></ul>",
+    "<p><strong>Please do NOT do any of the following things,<br>or you will be disqualified to prevent invalidating results:</strong></p>" +
+    "<ul><li>Hit ESC to exit fullscreen mode</li><li>Switch tabs or programs during the procedure</li><li>Zoom in or out</li><li>Reload the page once you have begun trials</li></ul>" +
+    "<p>You will be taken out of fullscreen mode after completing the study</p>",
 
     "<p>On each trial of this experiment you will see two video clips of<br>web pages loading back-to-back in <b>different browsers</b></p>" +
     "<p>Please pay close attention to how quickly the two pages load in the clips -<br>one browser <i>may</i> load the page more quickly than the other</p>",
@@ -119,7 +120,7 @@ var instructions_block = {
     "<p><i>Sometimes</i> there is no difference, though<br>That option is <u>also</u> available to respond with</p>",
     
     "<p>Rest your index fingers on the 'F' and 'J' keys and your thumbs on the space bar</p>" +
-    "<p>Press 'f' if the 1st clip loaded faster, 'j' if the 2nd clip loaded faster,<br>or space for the trials where the clips play at the same speed</p>" +
+    "<p>Press 'f' if the 1st clip loaded faster, 'j' if the 2nd clip loaded faster,<br>or space for the trials where the clips play at the same speed<br>(Answer screens remind you of the exact keys)</p>" +
     "<img src='{{ gamestatic('img/instAnswer.png') }}'></img> " + " <img src='{{ gamestatic('img/keyhands.jpg') }}'></img>" + "<p>Key responses keep the trials moving along quickly</p>",
 
     "<p>Error tones are provided for feedback after each response<br>Get familiar with the correct and incorrect sounds below and adjust your volume:<br>" +
